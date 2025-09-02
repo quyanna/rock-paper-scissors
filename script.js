4e9a5e78e6470a3f2f3f4db4e3aa3482b8ce7b3b
quyanna <quyannacampbell@gmail.com>
2025-09-02 14:11:33 -0700

    Add game functionality
    The game now plays 5 rounds of rock paper scissors against the computer.
    It outputs the result of each round, and at the end, outputs the final
    result and final scores all to the console.

--- script.js
+++ script.js
@@ -35,3 +35,71 @@ function getHumanChoice(message = "Enter your choice: ") {
       );
   }
 }
+
+function playGame() {
+  let humanScore = 0,
+    computerScore = 0;
+
+  for (i = 1; i <= 5; i++) {
+    console.log(`Round ${i}`);
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+  console.log("Game!");
+
+  if (humanScore == computerScore) {
+    console.log("It's a draw!");
+  } else if (humanScore < computerScore) {
+    console.log("You lose!");
+  } else {
+    console.log("You win!");
+  }
+
+  console.log(`Final scores: 
+    Computer: ${computerScore}
+    Human (you): ${humanScore}`);
+
+  //Plays a round of rock paper scissors
+  function playRound(humanChoice, computerChoice) {
+    let lose = function () {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+    };
+
+    let win = function () {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    };
+    if (computerChoice == humanChoice) {
+      console.log("It's a draw!");
+    } else if (humanChoice == ROCK) {
+      switch (computerChoice) {
+        case SCISSORS:
+          win();
+          break;
+        case PAPER:
+          lose();
+          break;
+      }
+    } else if (humanChoice == PAPER) {
+      switch (computerChoice) {
+        case SCISSORS:
+          lose();
+          break;
+        case ROCK:
+          win();
+          break;
+      }
+    } else if (humanChoice == SCISSORS) {
+      switch (computerChoice) {
+        case ROCK:
+          lose();
+          break;
+        case SCISSORS:
+          win();
+          break;
+      }
+    }
+  }
+}
+
+playGame();
