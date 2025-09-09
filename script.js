957a07b33c272cb7ab4b7952fe38479ad1937cdd
quyanna <quyannacampbell@gmail.com>
2025-09-09 14:19:51 -0700

    Complete refactor of event handling

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-/* 
-THIS AN AN EVENT HANDLING REFACTOR FOR THE ROCK PAPER SCISSORS GAME LOGIC
-
-STRUCTURE AND PLANNING: 
-
-- Create init function that sets up the game state and sets up listeners ONCE
-- Set up a global DOM Cache (global variables for storing DOM info)
-- Create a global game state object that contains: 
-  - Scores
-  - Whether game is over
-  - Max scores
-
-- FLOW
-StartGame function will clear the screen and show items
-EndGame will display end messages and hide items
-
-- Init Sets up event listeners and calls start game
-- Play round gets called whenever a round is played and calls functions to render scores 
-*/
-
 console.log("Hello, world!");
 
 const gameState = {
@@ -32,10 +12,13 @@ const nodes = {
     rockBtn: document.getElementById("rock-btn"),
     scissorsBtn: document.getElementById("scissors-btn"),
     paperBtn: document.getElementById("paper-btn"),
+    playAgainBtn: document.getElementById("play-again-btn"),
   },
   buttonsDiv: document.getElementById("buttons"),
   resultsDiv: document.getElementById("results"),
   scoreDiv: document.getElementById("score"),
+  winnerDiv: document.getElementById("winner"),
+  winnerMsg: document.getElementById("winner-msg"),
 };
 
 // Define the possible choices (for now)
@@ -43,7 +26,8 @@ const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
 
-// Utility functions
+/* -------- UTILITY FUNCTIONS --------*/
+
 function getComputerChoice() {
   const choices = [ROCK, PAPER, SCISSORS];
   return choices[Math.floor(Math.random() * choices.length)];
@@ -77,15 +61,43 @@ function renderRoundResult(text) {
   nodes.resultsDiv.replaceChildren(results);
 }
 
-//GAME LOGIC
+// Hides the game components and show winner if passed "true" or nothing, if passed false it unhides them
+function hideGame(hidden = true) {
+  if (hidden) {
+    nodes.buttonsDiv.classList.add("hidden");
+    nodes.resultsDiv.classList.add("hidden");
+    nodes.scoreDiv.classList.add("hidden");
+    nodes.winnerDiv.classList.remove("hidden");
+  } else {
+    nodes.buttonsDiv.classList.remove("hidden");
+    nodes.resultsDiv.classList.remove("hidden");
+    nodes.scoreDiv.classList.remove("hidden");
+    nodes.winnerDiv.classList.add("hidden");
+  }
+}
+
+/* -------- GAME LOGIC FUNCTIONS --------*/
+
 function startGame() {
   // reset state
   gameState.humanScore = 0;
   gameState.computerScore = 0;
-  isGameOver = false;
+
+  //If starting a new game, unhide hidden nodes and clear text from old game
+  if (gameState.isGameOver) {
+    hideGame(false);
+    nodes.resultsDiv.replaceChildren();
+    nodes.scoreDiv.replaceChildren();
+  }
+
+  gameState.isGameOver = false;
 }
 
-function endGame(winnerText) {}
+function endGame(winnerText) {
+  nodes.winnerMsg.textContent = winnerText;
+  hideGame();
+  gameState.isGameOver = true;
+}
 
 function playRound(humanChoice, computerChoice) {
   // IF game is not over
@@ -119,6 +131,8 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
+/* -------- INITIALIZATION AND EVENT LISTENER SETUP --------*/
+
 //Setup event listeners once
 function init() {
   nodes.buttons.rockBtn.addEventListener("click", () => {
@@ -130,147 +144,11 @@ function init() {
   nodes.buttons.scissorsBtn.addEventListener("click", () => {
     playRound(SCISSORS, getComputerChoice());
   });
+  nodes.buttons.playAgainBtn.addEventListener("click", () => {
+    startGame();
+  });
 
   startGame(); // Get game started
 }
 
 init();
-
-// function getComputerChoice() {
-//   let randomInt = Math.floor(Math.random() * CHOICES);
-//   switch (randomInt) {
-//     case 0:
-//       return ROCK;
-//     case 1:
-//       return PAPER;
-//     case 2:
-//       return SCISSORS;
-//   }
-// }
-
-// function playGame() {
-//   let humanScore = 0,
-//     computerScore = 0;
-
-//   //Add event listeners for each button
-//   const rockButton = document.querySelector(".rock-btn");
-//   const paperButton = document.querySelector(".paper-btn");
-//   const scissorsButton = document.querySelector(".scissors-btn");
-
-//   //Get ref to buttons div
-//   const buttons = document.querySelector(".buttons");
-
-//   //Get ref to results div
-//   const results = document.querySelector(".results");
-
-//   //Get ref to score div
-//   const runningScore = document.querySelector(".score");
-
-//   //Set up event listeners that call playRound when player makes a choice
-
-//   rockButton.addEventListener("click", () => {
-//     playRound(ROCK, getComputerChoice());
-//   });
-//   paperButton.addEventListener("click", () => {
-//     playRound(PAPER, getComputerChoice());
-//   });
-//   scissorsButton.addEventListener("click", () => {
-//     playRound(SCISSORS, getComputerChoice());
-//   });
-
-//   function playRound(humanChoice, computerChoice) {
-//     results.replaceChildren(); // Clear any results from last round
-//     const humanChoiceUI = document.createElement("div");
-//     const computerChoiceUI = document.createElement("div");
-
-//     humanChoiceUI.textContent = `You Chose: ${humanChoice}`;
-//     computerChoiceUI.textContent = `Computer Chose: ${computerChoice}`;
-
-//     results.appendChild(humanChoiceUI);
-//     results.appendChild(computerChoiceUI);
-
-//     // Functions for game control
-
-//     function lose() {
-//       computerScore++;
-//       console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-//       console.log(`Score is human:  ${humanScore} computer : ${computerScore}`);
-//     }
-
-//     function win() {
-//       humanScore++;
-//       console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-//       console.log(`Score is human:  ${humanScore} computer : ${computerScore}`);
-//     }
-
-//     function gameOverScreen(winnerText) {
-//       buttons.style.display = "none";
-//       runningScore.style.display = "none";
-//       results.replaceChildren();
-
-//       // Add winner message and Play Again button to results div
-//       displayWinner = document.createElement("div");
-//       displayWinner.classList.add("winner");
-//       displayWinner.textContent = winnerText;
-
-//       playAgainBtn = document.createElement("button");
-//       playAgainBtn.textContent = "Play Again?";
-//       playAgainBtn.classList.add("play-again-btn");
-//       displayWinner.appendChild(playAgainBtn);
-
-//       results.appendChild(displayWinner);
-
-//       // Listen for play again event
-//       playAgainBtn.addEventListener("click", () => {
-//         results.replaceChildren(); // clear results
-//         return;
-//       });
-//     }
-
-//     if (computerChoice == humanChoice) {
-//       console.log("It's a draw!");
-//     } else if (humanChoice == ROCK) {
-//       switch (computerChoice) {
-//         case SCISSORS:
-//           win();
-//           break;
-//         case PAPER:
-//           lose();
-//           break;
-//       }
-//     } else if (humanChoice == PAPER) {
-//       switch (computerChoice) {
-//         case SCISSORS:
-//           lose();
-//           break;
-//         case ROCK:
-//           win();
-//           break;
-//       }
-//     } else if (humanChoice == SCISSORS) {
-//       switch (computerChoice) {
-//         case ROCK:
-//           lose();
-//           break;
-//         case PAPER:
-//           win();
-//           break;
-//       }
-//     }
-
-//     //Call game over when anyone gets a score of 5
-//     if (humanScore >= 5) {
-//       gameOverScreen("You win!");
-//       return;
-//     } else if (computerScore >= 5) {
-//       gameOverScreen("You lose!");
-//       return;
-//     }
-
-//     // UPDATE RUNNING SCORE
-//     runningScore.textContent = `Human: ${humanScore}
-//   Computer: ${computerScore}`;
-//   }
-// }
-
-// playGame();
