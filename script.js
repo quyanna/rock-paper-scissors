b005484ee24f4233ce9fd6fec5d6ff0c952f347d
quyanna <quyannacampbell@gmail.com>
2025-09-09 13:17:19 -0700

    Refactored rendering behaviours, game runs

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ STRUCTURE AND PLANNING:
   - Whether game is over
   - Max scores
 
-- Phase 1: Set objects
+- FLOW
+StartGame function will clear the screen and show items
+EndGame will display end messages and hide items
+
+- Init Sets up event listeners and calls start game
+- Play round gets called whenever a round is played and calls functions to render scores 
 */
 
 console.log("Hello, world!");
@@ -19,16 +24,117 @@ const gameState = {
   humanScore: 0,
   computerScore: 0,
   isGameOver: false,
+  maxScore: 5,
 };
 
-const sectionNode = {};
+const nodes = {
+  buttons: {
+    rockBtn: document.getElementById("rock-btn"),
+    scissorsBtn: document.getElementById("scissors-btn"),
+    paperBtn: document.getElementById("paper-btn"),
+  },
+  buttonsDiv: document.getElementById("buttons"),
+  resultsDiv: document.getElementById("results"),
+  scoreDiv: document.getElementById("score"),
+};
 
-// // Define the possible choices (for now)
-// const ROCK = "rock";
-// const PAPER = "paper";
-// const SCISSORS = "scissors";
+// Define the possible choices (for now)
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
 
-// const CHOICES = 3;
+// Utility functions
+function getComputerChoice() {
+  const choices = [ROCK, PAPER, SCISSORS];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+//returns true iff a beats b, need to check for draw separately
+function beats(a, b) {
+  return (
+    (a == ROCK && b == SCISSORS) ||
+    (a == PAPER && b == ROCK) ||
+    (a == SCISSORS && b == PAPER)
+  );
+}
+
+function renderRunningScore() {
+  nodes.scoreDiv.replaceChildren(); // clear previously created divs
+
+  let compRunningScore = document.createElement("div");
+  let playerRunningScore = document.createElement("div");
+
+  playerRunningScore.textContent = `You: ${gameState.humanScore}`;
+  compRunningScore.textContent = `Computer: ${gameState.computerScore}`;
+
+  nodes.scoreDiv.appendChild(playerRunningScore);
+  nodes.scoreDiv.appendChild(compRunningScore);
+}
+
+function renderRoundResult(text) {
+  let results = document.createElement("div");
+  results.textContent = text;
+  nodes.resultsDiv.replaceChildren(results);
+}
+
+//GAME LOGIC
+function startGame() {
+  // reset state
+  gameState.humanScore = 0;
+  gameState.computerScore = 0;
+  isGameOver = false;
+}
+
+function endGame(winnerText) {}
+
+function playRound(humanChoice, computerChoice) {
+  // IF game is not over
+  // Check what beats what
+  //Increment scores
+  // Render scores
+  // Render result text
+  // Call endGame if game is over
+
+  if (!gameState.isGameOver) {
+    if (humanChoice == computerChoice) {
+      renderRoundResult(`Draw! Both players picked ${humanChoice}`);
+    } else if (beats(humanChoice, computerChoice)) {
+      renderRoundResult(`You Win! ${humanChoice} beats ${computerChoice}`);
+      gameState.humanScore++;
+    } else {
+      renderRoundResult(`You Lose! ${computerChoice} beats ${humanChoice}`);
+      gameState.computerScore++;
+    }
+    renderRunningScore();
+
+    // Check for a winner
+    if (gameState.humanScore >= 5 || gameState.computerScore >= 5) {
+      gameState.humanScore ==
+      Math.max(gameState.humanScore, gameState.computerScore)
+        ? endGame(`You Win! Final Score: 
+          You: ${gameState.humanScore} - Computer: ${gameState.computerScore}`)
+        : endGame(`You Lose! Final Score: 
+          You: ${gameState.humanScore} - Computer: ${gameState.computerScore}`);
+    }
+  }
+}
+
+//Setup event listeners once
+function init() {
+  nodes.buttons.rockBtn.addEventListener("click", () => {
+    playRound(ROCK, getComputerChoice());
+  });
+  nodes.buttons.paperBtn.addEventListener("click", () => {
+    playRound(PAPER, getComputerChoice());
+  });
+  nodes.buttons.scissorsBtn.addEventListener("click", () => {
+    playRound(SCISSORS, getComputerChoice());
+  });
+
+  startGame(); // Get game started
+}
+
+init();
 
 // function getComputerChoice() {
 //   let randomInt = Math.floor(Math.random() * CHOICES);
